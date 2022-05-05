d6b9c5946e5ff344f65bab4ec6f5271dd4c8e2af
Vincent Zhang <ivincent.zhang@gmail.com>
2022-05-05 20:26:35 +0800

    Fix: 1. simplify pom.xml Imps: 1. add .gitattributes

--- exotic-app/exotic-ML-examples/src/main/kotlin/ai/platon/exotic/examples/sites/topEc/english/shopee/ShopeeHarvester.kt
+++ exotic-app/exotic-ML-examples/src/main/kotlin/ai/platon/exotic/examples/sites/topEc/english/shopee/ShopeeHarvester.kt
@@ -4,8 +4,12 @@ import ai.platon.exotic.examples.common.VerboseHarvester
 
 fun main() {
     val portalUrl = "https://shopee.sg/Computers-Peripherals-cat.11013247"
-    val args = "-i 1s -ii 1d -ol a[href~=sp_atk] -tl 20 -ignoreFailure"
+    val args = "-i 10d -ii 10d -ol a[href~=sp_atk] -tl 20 -ignoreFailure" +
+//            " -itemRequireSize 200000 -itemScrollCount 30"
+            " -component .page-product__breadcrumb" +
+            " -component .product-briefing" +
+            " -diagnose -vj"
 
-    val harvester = VerboseHarvester()
-    harvester.harvest(portalUrl, args)
+    val crawler = VerboseHarvester()
+    crawler.harvest(portalUrl, args)
 }
--- exotic-app/exotic-examples/src/main/kotlin/ai/platon/exotic/examples/sites/topEc/english/shopee/ShopeeCrawler.kt
+++ exotic-app/exotic-examples/src/main/kotlin/ai/platon/exotic/examples/sites/topEc/english/shopee/ShopeeCrawler.kt
@@ -1,23 +1,33 @@
 package ai.platon.exotic.examples.sites.topEc.english.shopee
 
+import ai.platon.pulsar.browser.common.BrowserSettings
 import ai.platon.pulsar.context.PulsarContexts
+import ai.platon.pulsar.crawl.common.url.ParsableHyperlink
+import ai.platon.pulsar.dom.select.selectHyperlinks
+import ai.platon.pulsar.persist.WebPage
+import org.jsoup.nodes.Document
 
 fun main() {
-    val portalUrl = "https://shopee.sg/Computers-Peripherals-cat.11013247"
-    val args = "-i 1s -ii 1d -ol a[href~=sp_atk] -tl 20 -ignoreFailure"
-    val session = PulsarContexts.createSession()
+    BrowserSettings.withWorseNetwork()
 
-//    val fieldSelectors = listOf("._2Csw3W", "._3uBhVI", "._3b2Btx", "._1kpF5Y")
-    val fieldSelectors = mapOf(
-        "title" to "._2Csw3W",
-        "price" to "._3uBhVI",
-        "star" to "._3b2Btx",
-        "ratings" to "._1kpF5Y"
-    )
-    // the result seems to change frequently, here are the history records:
-    // 2022-04-27
-    // [{title=$19.90, price=4.9, star=8k, ratings=65% off}, {title=$1.99 - $5.28, price=4.9, star=5.3k, ratings=89% off}, {title=$12.38 - $15.00, price=4.9, star=1.2k, ratings=41% off}, {title=$19.90 - $39.90, price=4.9, star=29k, ratings=62% off}, {title=$10.65 - $18.99, price=4.9, star=11.1k, ratings=58% off}, {title=$5.02 - $10.00, price=4.9, star=7.5k, ratings=70% off}, {title=$6.70 - $14.10, price=4.8, star=3.6k, ratings=53% off}, {title=$23.99 - $29.99, price=4.9, star=3.9k, ratings=58% off}, {title=$3.06, price=5.0, star=153, ratings=3% off}, {title=$24.99 - $39.99, price=4.9, star=15.6k, ratings=72% off}, {title=$8.50, price=5.0, star=3.1k, ratings=31% off}, {title=null, price=4.7, star=4.1k, ratings=null}, {title=$3.99 - $11.99, price=4.8, star=20.4k, ratings=52% off}, {title=$1.89, price=5.0, star=31.4k, ratings=84% off}, {title=null, price=null, star=null, ratings=null}, {title=$3.79 - $9.90, price=4.8, star=10.7k, ratings=50% off}, {title=null, price=null, star=null, ratings=null}, {title=null, price=4.9, star=2.4k, ratings=null}, {title=$4.50 - $7.50, price=4.9, star=3.9k, ratings=49% off}, {title=null, price=4.8, star=2.6k, ratings=null}]
-    //
-    val fields = session.scrapeOutPages(portalUrl, args, fieldSelectors)
-    println(fields)
+    val portalUrls = """
+        https://shopee.sg/Mobile-Gadgets-cat.11013350
+        https://shopee.sg/Home-Appliances-cat.11027421
+        https://shopee.sg/Food-Beverages-cat.11011871
+        https://shopee.sg/Kids-Fashion-cat.11012218
+        https://shopee.sg/Women's-Apparel-cat.11012819
+        https://shopee.sg/Sports-Outdoors-cat.11012018
+    """.trimIndent().split("\n").map { it.trim() }.map { "$it?page={pageNumber}" }
+        .flatMap { url -> IntRange(1, 8).map { url.replace("{pageNumber}", "$it") } }
+        .shuffled()
+    val args = "-i 1s -ignoreFailure -refresh"
+
+    val context = PulsarContexts.create()
+
+    val parseHandler = { _: WebPage, document: Document ->
+        context.submitAll(document.selectHyperlinks("a[href~=sp_atk]"))
+    }
+
+    val links = portalUrls.map { ParsableHyperlink("$it $args", parseHandler) }
+    context.submitAll(links).await()
 }
