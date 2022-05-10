6ac228eb849e9efd9c129f5b046e01f7b47a91d4
Vincent Zhang <ivincent.zhang@gmail.com>
2022-05-11 01:27:37 +0800

    Fix: 1. next-minor.sh Upgrade: 1. pulsar to 1.9.6 2. scent to 1.9.0

--- bin/release/next-minor.sh
+++ bin/release/next-minor.sh
@@ -10,13 +10,18 @@ PREFIX=$(echo "$VERSION" | cut -d'.' -f1,2)
 MINOR_VERSION=$(echo "$VERSION" | cut -d'.' -f3)
 MINOR_VERSION=$(("$MINOR_VERSION" + 1))
 
-NEXT_VERSION="$PREFIX.$MINOR_VERSION-SNAPSHOT"
+NEXT_VERSION="$PREFIX.$MINOR_VERSION"
+NEXT_SNAPSHOT_VERSION="$NEXT_VERSION-SNAPSHOT"
 
-echo "New version: $NEXT_VERSION"
-echo "$NEXT_VERSION" > "$APP_HOME"/VERSION
-find "$APP_HOME" -name 'pom.xml' -exec sed -i "s/$SNAPSHOT_VERSION/$NEXT_VERSION/" {} \;
+echo "New version: $NEXT_SNAPSHOT_VERSION"
+# VERSION file
+echo "$NEXT_SNAPSHOT_VERSION" > "$APP_HOME"/VERSION
+# README
+sed -i "s/\b$PREFIX.[0-9]\{1,\}\b/$NEXT_VERSION/g" "$APP_HOME/README.adoc";
+# pom.xml files
+find "$APP_HOME" -name 'pom.xml' -exec sed -i "s/$SNAPSHOT_VERSION/$NEXT_SNAPSHOT_VERSION/" {} \;
 
-COMMENT="v"${$NEXT_VERSION//"-SNAPSHOT"/""}
+COMMENT=${NEXT_SNAPSHOT_VERSION//"-SNAPSHOT"/""}
 
 echo "Ready to commit with comment: <$COMMENT>"
 read -p "Are you sure to continue?" -n 1 -r
