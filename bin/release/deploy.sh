36862038d6143ab86791cda3d8a356c858d52a77
Vincent Zhang <ivincent.zhang@gmail.com>
2022-05-16 16:32:23 +0800

    Fix: 1. fix scripts

--- bin/release/deploy.sh
+++ bin/release/deploy.sh
@@ -16,10 +16,12 @@ VERSION=${SNAPSHOT_VERSION//"-SNAPSHOT"/""}
 echo "$VERSION" > "$APP_HOME"/VERSION
 find "$APP_HOME" -name 'pom.xml' -exec sed -i "s/$SNAPSHOT_VERSION/$VERSION/" {} \;
 
-mvn clean
-mvn
+#mvn clean
+#mvn
 
-REMOTE_BASE_DIR=~/platonic.fun/repo/ai/platon/exotic
+REMOTE_BASE_DIR=~/platonic.fun/repo/ai/platon/exotic/
+ssh $HOST mkdir -p $REMOTE_BASE_DIR
+
 scp -r "$APP_HOME"/exotic-standalone/target/exotic-standalone-"$VERSION".jar "$HOST:$REMOTE_BASE_DIR"
 
 echo "List directory before creating symbolic link: "
