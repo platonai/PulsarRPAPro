d7fea773abe26ffb57690d750e290a396034bc73
galaxyeye <ivincent.zhang@gmail.com>
2022-11-24 12:37:05 +0800

    Imps: 1. improve start-copy Docs: 1. update README

--- bin/tools/chrome/prototype/start-copy.sh
+++ bin/tools/chrome/prototype/start-copy.sh
@@ -8,8 +8,8 @@ userDataDir="/tmp/pulsar-$USER/context/browser$now"
 if [ ! -e "$userDataDir" ]; then
   echo "Copy data from $prototypeDataDir to $userDataDir"
   cp -r "$prototypeDataDir" "$userDataDir"
-  rm "$userDataDir/SingletonCookie"
-  rm "$userDataDir/SingletonLock"
+  rm -r "$userDataDir/SingletonCookie"
+  rm -r "$userDataDir/SingletonLock"
   unlink "$userDataDir/SingletonSocket"
 fi
 
