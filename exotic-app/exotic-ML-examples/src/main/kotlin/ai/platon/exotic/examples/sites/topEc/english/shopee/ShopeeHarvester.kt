8e4cc58d3cc48097b8862af836fb92036a11f8d3
galaxyeye <ivincent.zhang@gmail.com>
2023-05-29 11:52:55 +0800

    Upgrade: 1. scent to 1.10.6

--- exotic-app/exotic-ML-examples/src/main/kotlin/ai/platon/exotic/examples/sites/topEc/english/shopee/ShopeeHarvester.kt
+++ exotic-app/exotic-ML-examples/src/main/kotlin/ai/platon/exotic/examples/sites/topEc/english/shopee/ShopeeHarvester.kt
@@ -3,11 +3,9 @@ package ai.platon.exotic.examples.sites.topEc.english.shopee
 import ai.platon.exotic.examples.common.VerboseHarvester
 
 fun main() {
-    val portalUrl = "https://shopee.sg/Computers-Peripherals-cat.11013247"
-    val args = "-ol a[href~=sp_atk] -tl 20 -ignoreFailure" +
-            " -itemRequireSize 200000 -itemScrollCount 30"
-            " -component .page-product__breadcrumb" +
-            " -component .product-briefing" +
+    val portalUrl = "https://shopee.sg/Laptops-cat.11013247.11013305"
+    val args = "-ol a[href~=sp_atk] -tl 40 -ignoreFailure" +
+            " -itemRequireSize 200000 -itemScrollCount 30" +
             " -diagnose -vj"
 
     VerboseHarvester().harvest(portalUrl, args)
