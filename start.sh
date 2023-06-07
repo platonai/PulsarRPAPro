c54da7f7d753f3bed81b63018d8641e5242ea066
galaxyeye <ivincent.zhang@gmail.com>
2023-06-07 18:17:34 +0800

    Imps: 1. add start script

---
+++ start.sh
@@ -0,0 +1,8 @@
+FILE_COUNT=$(find "exotic-standalone/target/" -wholename "exotic-standalone*.jar" | wc -l)
+
+if (( FILE_COUNT == 0 )); then
+  mvn -DskipTests=true
+fi
+
+cd exotic-standalone/target/ || exit
+java -jar exotic-standalone*.jar serve
