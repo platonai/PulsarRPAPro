8286c9df313f190c35bc2c1d0bcaade99acc04d7
PlatonAI <galaxy.eye@qq.com>
2023-10-20 11:39:32 +0800

    Upgrade: 1. spring 2.5.4 -> 2.5.15 2. pulsar to 1.10.22, scent to 1.11.1 Feature: 1. add exotic-PERF-examples to demonstrate the crawl performance Fix: 1. fix harvest process startup failure on Windows (fix on scent-1.11.1)

--- DEVPLAN.md
+++ DEVPLAN.md
@@ -2,6 +2,7 @@
 
 ## 1.12.x
 
-### Dependency upgrading
-
-1. spring 2.5.4 -> 2.6.12
+1. spring 2.5.4 -> 2.5.15
+2. pulsar to 1.10.22, scent to 1.11.1
+3. add [exotic-PERF-examples](exotic-app/exotic-PERF-examples) to demonstrate the crawl performance
+4. fix harvest process startup failure on Windows (fix on scent-1.11.1)
---
+++ exotic-app/exotic-PERF-examples/src/main/kotlin/ai/platon/exotic/examples/sites/proxy/vendor/ProxyVendorFactory.kt
@@ -0,0 +1,32 @@
+package ai.platon.exotic.examples.sites.proxy.vendor
+
+import ai.platon.exotic.examples.sites.proxy.vendor.zm.ZMProxyParser
+import ai.platon.pulsar.common.proxy.ProxyEntry
+import org.slf4j.LoggerFactory
+import java.nio.file.Files
+import java.nio.file.Path
+
+abstract class ProxyParser {
+    companion object {
+        const val IPADDRESS_PATTERN = "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+    }
+
+    val logger = LoggerFactory.getLogger(ProxyParser::class.java)
+    abstract fun parse(text: String, format: String): List<ProxyEntry>
+    open fun parse(path: Path, format: String): List<ProxyEntry> = parse(Files.readString(path), format)
+}
+
+class DefaultProxyParser: ProxyParser() {
+    override fun parse(text: String, format: String): List<ProxyEntry> {
+        return text.split("\n").mapNotNull { ProxyEntry.parse(text) }
+    }
+}
+
+object ProxyVendorFactory {
+    fun getProxyParser(vendor: String): ProxyParser {
+        return when (vendor) {
+            "zm" -> ZMProxyParser()
+            else -> DefaultProxyParser()
+        }
+    }
+}
