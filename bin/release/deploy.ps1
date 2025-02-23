d13e169e6f4b55e1083a53047b36d2afebb2ad82
platon.AI <galaxyeye@live.cn>
2025-02-23 21:53:53 +0800

    Feature: 1. add deploy.ps1

---
+++ bin/release/deploy.ps1
@@ -0,0 +1,82 @@
+# Find the first parent directory containing the VERSION file
+$AppHome=(Get-Item -Path $MyInvocation.MyCommand.Path).Directory
+while ($AppHome -ne $null -and !(Test-Path "$AppHome/VERSION")) {
+  $AppHome=$AppHome.Parent
+}
+cd $AppHome
+
+function printUsage {
+  Write-Host "Usage: deploy.ps1 [-clean|-test]"
+  exit 1
+}
+
+# Maven command and options
+$MvnCmd = Join-Path $AppHome '.\mvnw.cmd'
+
+# Initialize flags and additional arguments
+$PerformClean = $false
+$SkipTests = $true
+
+$AdditionalMvnArgs = @()
+
+# Parse command-line arguments
+foreach ($Arg in $args)
+{
+  switch ($Arg)
+  {
+    '-clean' {
+      $PerformClean = $true;
+    }
+    { '-t', '-test' } {
+      $SkipTests = $false;
+    }
+    { $_ -in "-h", "-help", "--help" } {
+      printUsage
+    }
+    { $_ -in "-*", "--*" } {
+      printUsage
+    }
+    Default {
+      $AdditionalMvnArgs += $Arg
+    }
+  }
+}
+
+Write-Host "Deploy the project ..."
+Write-Host "Changing version ..."
+
+$SNAPSHOT_VERSION = Get-Content "$AppHome\VERSION" -TotalCount 1
+$VERSION =$SNAPSHOT_VERSION -replace "-SNAPSHOT", ""
+$VERSION | Set-Content "$AppHome\VERSION"
+
+# Replace SNAPSHOT version with the release version
+@('README.md', 'README-CN.md', 'pom.xml') | ForEach-Object {
+  Get-ChildItem -Path "$AppHome" -Depth 2 -Filter $_ -Recurse | ForEach-Object {
+    (Get-Content $_.FullName) -replace $SNAPSHOT_VERSION, $VERSION | Set-Content $_.FullName
+  }
+}
+
+if ($PerformClean) {
+  & $MvnCmd clean -Pall-modules
+  if ($LastExitCode -ne 0) {
+    exit $LastExitCode
+  }
+}
+
+if ($SkipTests) {
+  & $MvnCmd deploy -Pplaton-release -Pplaton-deploy -DskipTests
+} else {
+  & $MvnCmd deploy -Pplaton-release -Pplaton-deploy
+}
+
+$exitCode =$LastExitCode
+if ($exitCode -eq 0) {
+  Write-Host "Build successfully"
+} else {
+  exit $exitCode
+}
+
+Write-Host "Artifacts are staged remotely, you should close and release the staging manually:"
+Write-Host "https://oss.sonatype.org/#stagingRepositories"
+Write-Host "Hit the following link to check if the artifacts are synchronized to the maven center: "
+Write-Host "https://repo1.maven.org/maven2/ai/platon/pulsar"
