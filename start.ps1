62017b4f80c7dd0998673b38d473b98ddf633cc8
platon.AI <galaxyeye@live.cn>
2025-05-08 08:15:16 +0800

    refactor(exotic-standalone): 重构启动脚本和帮助文档
    - 优化了 ExoticExecutor.kt 文件中的代码结构和逻辑
    - 重新编写了 help.ps1 脚本，提高了可读性和健壮性
    -简化了 start.ps1脚本，去除了不必要的 Java 版本检查和 JVM 选项

--- start.ps1
+++ start.ps1
@@ -6,13 +6,6 @@ while ($AppHome -ne $null -and !(Test-Path "$AppHome/VERSION")) {
 }
 cd $AppHome
 
-# Check java version, make sure it is Java 11
-$JAVA_VERSION = (java -version 2>&1 | Select-String "version" | Select-String "11\.")
-
-if ($JAVA_VERSION -eq $null) {
-  Write-Output "WARNING: Java 11 is required to run this program"
-}
-
 $FILES=(Get-ChildItem -Path "$AppHome/exotic-standalone/target/" -Filter "PulsarRPAPro.jar" -Recurse)
 $FILE_COUNT = ($FILES | Measure-Object).Count
 
@@ -24,11 +17,6 @@ $JAR=(Resolve-Path $FILES[0])
 
 $URL = $args[0]
 $args = $args[1..($args.Length - 1)]
-
-# --add-opens java.base/java.time=ALL-UNNAMED to fix JEP 396: Strongly Encapsulate JDK Internals by Default,
-# the problem appears when upgrading java from 11 to 17.
-# $JVM_OPTS = "--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.time=ALL-UNNAMED"
-# $JVM_OPTS = "--add-exports=java.naming/com.sun.jndi.ldap=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.invoke=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.security=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.management/javax.management=ALL-UNNAMED --add-opens=java.naming/javax.naming=ALL-UNNAMED"
 
 Write-Output "java $JVM_OPTS -jar $JAR serve $args"
 
